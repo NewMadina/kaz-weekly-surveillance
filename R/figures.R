905d18e3e807052351cf0f513b4f6b5126d02e12
Nishant Kishore <ynm2@cdc.gov>
2022-12-14 00:12:32 -0500

    Update figures.R

--- R/figures.R
+++ R/figures.R
@@ -58,7 +58,7 @@ z <- kaz$afp |>
   complete(month, year, prov) |>
   arrange(prov, year, month) %>% 
   replace_na(list("count" = 0)) |>
-  left_join(test_pop, 
+  left_join(kaz$pop_extrapolated, 
             by = c("prov" = "name", "year", "month")) %>% 
   mutate(afp_rate = count / value * 10000) |> 
   drop_na() |>
@@ -88,15 +88,12 @@ fig2 <- z |>
   geom_tile(aes(x = date, y = prov, fill = afp_rate), 
             color = "white", size = 0.02) + 
   #scale_fill_viridis_d(option = "plasma") +
- #scale_fill_brewer(palette = "Reds", direction = 1) +
-  scale_fill_brewer(palette = "Diamond\nclarity", direction = 1) +
+  scale_fill_brewer(palette = "Reds", direction = 1) +
+  #scale_fill_brewer(palette = "Diamond\nclarity", direction = 1) +
   theme_classic() + 
-  theme(
-    axis.text.x = element_text(angle = 90, hjust = 1)
-  )+
-  labs (fill = "Показатель заболеваемости", y = "Территории", 
-       title = "Динамика заболеваемости полиомиелитом, РК, 2013-2022 гг.", 
-       caption = "Не смотря на выявление ОВП в большинстве территорий, /nсуществуют несколько территорий с нулевым показателем заболеваемости ОВП в течении последних 2 лет")
+  labs (fill = "Показатель\nзаболеваемости\nОВП", y = "Территории", x = "Дата",
+       title = "Динамика заболеваемости ОВП, РК, 2013-2022 гг.", 
+       caption = "Несмотря на выявление ОВП в большинстве территорий, существуют несколько территорий с нулевым показателем заболеваемости ОВП в течении последних 2 лет")
 
 
 ggsave(here("output/fig2.png"), fig2, height = 3, width = 12, dpi = 300)
